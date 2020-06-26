eb73d77e5ef2cc2f0a07d9cb02ab0fad67626ccc
Virus <vladprodan6@gmail.com>
2020-06-26 18:00:41 +0300

    +

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,38 @@
 
-$(document).ready(function() {
-  $('.nav-toggle').click(function() {
-    var collapse_content_selector = $(this).attr('href');
-    var toggle_switch = $(this);
-    $(collapse_content_selector).toggle(function() {
-      if ($(this).css('display') == 'none') {
-        toggle_switch.html('View More');
-      } else {
-        toggle_switch.html('View Less');
+// Slider
+
+$('.response').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1920,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
       }
-    });
-  });
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
