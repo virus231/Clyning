d3f9a3c3d104c4618a7a974869a7e2752bca177a
Virus <vladprodan6@gmail.com>
2020-06-27 23:31:55 +0300

    +++

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,50 @@
 $("#box").twentytwenty();
 
 
+$('#headingOne1').click(function (e) {
+    e.preventDefault()
+    $(this).find('.arrow').toggleClass('active');
+  });
+  
+  $('#headingTwo2').click(function (e) {
+    e.preventDefault()
+  
+    $(this).find('.arrow').toggleClass('active');
+  });
+  
+  $('#headingTwo').click(function (e) {
+    e.preventDefault()
+  
+    $(this).find('.arrow').toggleClass('active');
+  });
+  
+  $('#headingFour').click(function (e) {
+    e.preventDefault()
+  
+    $(this).find('.arrow').toggleClass('active');
+  });
+  
+  $('#headingThree4').click(function (e) {
+    e.preventDefault()
+  
+    $(this).find('.arrow').toggleClass('active');
+  });
+  
+  $('#headingThree6').click(function (e) {
+    e.preventDefault()
+  
+    $(this).find('.arrow').toggleClass('active');
+  });
+  
+  $('#headingFour123').click(function (e) {
+    e.preventDefault()
+  
+    $(this).find('.arrow').toggleClass('active');
+  });
+  
+  $('#headingThree7').click(function (e) {
+    e.preventDefault()
+    $(this).find('.arrow').toggleClass('active');
+  });
+
 
